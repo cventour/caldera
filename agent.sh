d6bcbf90271f895063083abaad77ffc81bd8a5e5
Christos Ventouris <ventouris@gmail.com>
2021-02-17 11:57:22 +0400

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 10
 server="http://127.0.0.1:8888"
 agent=$(curl -svkOJ -X POST -H "file:sandcat.go" -H "platform:darwin" $server/file/download 2>&1 | grep -i "Content-Disposition" | grep -io "filename=.*" | cut -d'=' -f2 | tr -d '"\r') && chmod +x $agent 2>/dev/null
 nohup ./$agent -server $server &
